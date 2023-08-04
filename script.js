eb6d7162537539a7023fe2634c0ab8c2428e2fb6
victordaviavilaa <124811608+victordaviavilaa@users.noreply.github.com>
2023-08-04 16:13:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function trocar1(){
+    document.body.style.backgroundImage="url('https://i.pinimg.com/736x/99/bc/d3/99bcd3b0ed0dc3729dfe5e45780ef5bf.jpg')";
+}
+function trocar2(){
+    document.body.style.backgroundImage="url('https://i.pinimg.com/550x/60/6e/dd/606edd697669c31370e863489df0fd4f.jpg')";
+}
+function trocar3(){
+    document.body.style.backgroundImage="url('https://i.pinimg.com/1200x/63/37/d8/6337d8af6cacc5cecb8e572461dfd9f1.jpg')";
+}
